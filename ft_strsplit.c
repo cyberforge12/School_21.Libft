1e3f9491b8a8891f877dc87fd3d0a77f906f46d4
mmanhack <no>
2019-10-17 16:58:29 +0300

    fixed after norminette check, removed author for further projects

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,21 +6,20 @@
 /*   By: jconcent <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/11 12:29:07 by jconcent          #+#    #+#             */
-/*   Updated: 2019/09/16 16:59:42 by jconcent         ###   ########.fr       */
+/*   Updated: 2019/10/17 16:57:15 by mmanhack         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char		**ft_split_creator(char const *s, char c,
-						int word_count, int max_word_len)
+static char	**ft_split_creator(char const *s, char c, int words, int max_len)
 {
 	char	**sp_s;
 	int		i;
 	int		j;
 	int		k;
 
-	if (!(sp_s = (char **)malloc(sizeof(*sp_s) * (word_count + 1))))
+	if (!(sp_s = (char **)malloc(sizeof(*sp_s) * (words + 1))))
 		return (NULL);
 	i = 0;
 	j = 0;
@@ -31,8 +30,7 @@ static char		**ft_split_creator(char const *s, char c,
 			i++;
 		if (s[i] != c && s[i])
 		{
-			if (!(sp_s[j] = (char *)malloc(sizeof(**sp_s) * (max_word_len +
-					1))))
+			if (!(sp_s[j] = (char *)malloc(sizeof(**sp_s) * (max_len + 1))))
 				return (ft_clear_table(sp_s, j));
 			while (s[i] != c && s[i])
 				sp_s[j][k++] = s[i++];
@@ -43,7 +41,7 @@ static char		**ft_split_creator(char const *s, char c,
 	return (sp_s);
 }
 
-char			**ft_strsplit(char const *s, char c)
+char		**ft_strsplit(char const *s, char c)
 {
 	int word_count;
 	int word_len;
